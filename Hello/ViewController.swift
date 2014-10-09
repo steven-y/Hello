355000e73556745411b77753bc522cbd8650e430
Yan Zhanyi <yanzhanyi@YandeMacBook-Pro.local>
2014-10-09 12:07:31 +0800

    Initial Commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Yan Zhanyi on 14-10-9.
+//  Copyright (c) 2014年 Yan Zhanyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
